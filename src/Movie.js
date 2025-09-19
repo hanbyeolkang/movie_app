17424bc50dc85cc5e22ae0148807a928ccfb28f0
hanbyeolkang <strongstar1210@gmail.com>
2025-09-19 13:44:34 +0900

    파이썬 실습을 위해 id 값 추가

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
     return(
-        <div className="movie">
+        <div id={id} className="movie">
             <img src={poster} alt={title} title={title} />
             <div className="movie__data">
                 <h3 className="movie__title">{title}</h3>
